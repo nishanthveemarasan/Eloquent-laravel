18077aaf6a7e6876590d8f54a5b236cfd249426c
nishanthveema <iamnishanthveema@gmail.com>
2021-02-22 07:53:19 +0530

    cache

--- app/Http/Controllers/ModelController.php
+++ app/Http/Controllers/ModelController.php
@@ -3,18 +3,22 @@
 namespace App\Http\Controllers;
 
 use App\Models\City;
-use App\Models\Customer;
-use App\Models\Payment;
 use App\Models\Upload;
+use App\Models\Payment;
+use App\Models\Customer;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Cache;
 
 class ModelController extends Controller
 {
     public function modelGet()
     {
-        $getPayment = Payment::first();
-        dd($getPayment->attributesToArray());
+        $getPayment = Payment::first()->toArray();
+        $array = array('a' => 'b');
+        $time = now()->addMinutes(30);
+        Cache::forget('key');
+        dd(Cache::get('key'));
     }
 
     public function writeCode()
